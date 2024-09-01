ab26973864beeae698721aa188e37ce2424460a2
Patrick Sanan <psanan@google.com>
2024-08-31 19:02:50 -0700

    Stub of entry filler inner

--- process_template.py
+++ process_template.py
@@ -2,9 +2,26 @@
 
 import file_io
 
+# FIXME duplicated helper
+def _entry_field_as_list(entry, field):
+    if not isinstance(entry, dict):
+        print(type(entry))
+        raise RuntimeError()
+    field = entry[field]
+    return field if isinstance(field, list) else [field]
+
+def _entry_to_html(entry):
+    # TODO make prettier
+    entry_ch = _entry_field_as_list(entry, "ch")[0]
+    if "en" in entry:
+        entry_en = _entry_field_as_list(entry, "en")[0]
+    else:
+        entry_en = ""
+    return f"<li> <b>{entry_ch}</b> : {entry_en}"
+
 def process_template(path, entries_by_ch):
-    # TODO: add a printer for entries and build the filled-in version of the template
     found_entries_by_ch = {}
+    lines_out = []
     with open(path, "r") as template_file:
         for line in template_file:
             line_stripped = line.strip()
@@ -15,8 +32,16 @@ def process_template(path, entries_by_ch):
                     entry = entries_by_ch[ch]
                     print("Found entry by ch: ", ch, entry)
                     found_entries_by_ch[ch] = entry
+                    lines_out.append(_entry_to_html(entry))
                 else:
                     print("WARNING: didn't find entry for ", ch_potential)
+                    lines_out.append(line)
+            else:
+                    lines_out.append(line)
+    out_path = path + ".out.html"
+    with open(out_path, "w") as out_file:
+        for line in lines_out:
+            out_file.write(line)
     return found_entries_by_ch
 
 
